a552465ba5100eff907be06f48eec50533479c63
yucheng2 <17835204735@163.com>
2026-05-23 08:42:51 +0800

    fix: quote custom element test stub

--- tests/setup.ts
+++ tests/setup.ts
@@ -5,7 +5,7 @@ config.global.stubs = {
   view: { template: '<div><slot /></div>' },
   text: { template: '<span><slot /></span>' },
   image: { template: '<img :src="src" :alt="alt" @click="$emit(\'click\', $event)" />', props: ['src', 'alt'] },
-  scroll-view: { template: '<div><slot /></div>' },
+  'scroll-view': { template: '<div><slot /></div>' },
   button: { template: '<button :disabled="disabled" @click="$emit(\'click\', $event)"><slot /></button>', props: ['disabled'] },
   input: { template: '<input :value="modelValue" @input="$emit(\'update:modelValue\', ($event.target as HTMLInputElement).value)" />', props: ['modelValue'] }
 }
